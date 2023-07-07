00a1a65702c071606b5cd11abc6d1403aa87dcd5
Murtaza18 <murtazatuta72@gmail.com>
2023-07-08 01:57:41 +0300

    Excercise 3: Making the vertices as the color of triangle(Bottom left is black because -ve values in color are made 0.0f and therefore it is black)

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -52,9 +52,6 @@ int main(void)
 
 		ourShader.use();
 
-		int offsetUniformLoc = glGetUniformLocation(ourShader.ID, "offset");
-		glUniform3f(offsetUniformLoc, 0.3, 0.0, 0.0);
-
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
