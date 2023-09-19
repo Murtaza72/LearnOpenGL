448a6f894dc1c06de1c06691005f3570280870e2
Murtaza18 <murtazatuta72@gmail.com>
2023-09-19 23:28:55 +0530

    Textures work properly at last, I bound the VAO before VBO and IBO

--- LearnOpenGL/src/tests/TestTextures.cpp
+++ LearnOpenGL/src/tests/TestTextures.cpp
@@ -15,8 +15,8 @@ namespace test
 		m_ImageBuffer(nullptr),
 		m_Shader("res/shaders/texture.vs", "res/shaders/texture.fs")
 	{
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
 		float vertices[] = {
 			// positions       // texture coords
@@ -31,51 +31,55 @@ namespace test
 			1, 2, 3
 		};
 
-		glGenBuffers(1, &m_VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		GLCall(glGenVertexArrays(1, &m_VAO));
+		GLCall(glBindVertexArray(m_VAO));
 
-		glGenBuffers(1, &m_IBO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		GLCall(glGenBuffers(1, &m_VBO));
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_VBO));
+		GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
 
-		glGenBuffers(1, &m_VAO);
-		glBindVertexArray(m_VAO);
+		GLCall(glGenBuffers(1, &m_IBO));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO));
+		GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		GLCall(glEnableVertexAttribArray(0));
+		GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0));
 
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		GLCall(glEnableVertexAttribArray(1));
+		GLCall(glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))));
 
 		m_TextureID = LoadTexture("res/textures/me.png");
+
+		m_Shader.use();
+		m_Shader.setInt("tex1", 0);
 	}
 
 	TestTextures::~TestTextures()
 	{
+		GLCall(glDeleteVertexArrays(1, &m_VAO));
+		GLCall(glDeleteBuffers(1, &m_VBO));
+		GLCall(glDeleteVertexArrays(1, &m_IBO));
+
+		GLCall(glBindTexture(GL_TEXTURE_2D, 0))
 	}
 
 	void TestTextures::OnRender(Camera camera)
 	{
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, m_TextureID);
+		GLCall(glActiveTexture(GL_TEXTURE0));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_TextureID));
 
-		m_Shader.use();
-		m_Shader.setInt("tex1", 0);
-
-		glBindVertexArray(m_VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		GLCall(glBindVertexArray(m_VAO));
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
 	}
 
 	void TestTextures::OnImGuiRender()
 	{
-
 	}
 
 	unsigned int TestTextures::LoadTexture(const std::string& path)
 	{
 		unsigned int textureID;
-		GLCall(glGenTextures(1, &textureID));
+		GLCall(GLCall(glGenTextures(1, &textureID)));
 
 		stbi_set_flip_vertically_on_load(true);
 
