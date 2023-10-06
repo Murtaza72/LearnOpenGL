0c49ddf13ccc07620ec74bcdbc0651a429ca9540
Murtaza18 <murtazatuta72@gmail.com>
2023-10-06 19:17:03 +0530

    Changed the definition of GLLogCall()

--- LearnOpenGL/src/Utils.cpp
+++ LearnOpenGL/src/Utils.cpp
@@ -5,11 +5,73 @@ void GLClearError()
 	while (glGetError() != GL_NO_ERROR);
 }
 
-bool GLLogCall(const char* function, const char* file, int line)
+bool GLLogCall(std::string_view function, const char* file, int line)
 {
-	while (GLenum error = glGetError())
+	// Get the last error
+	const GLenum errorCode = glGetError();
+
+	if (errorCode != GL_NO_ERROR)
 	{
-		std::cout << "[OpenGl Error] (" << error << "): " << function << " " << file << ": " << line << std::endl;
+		std::string error = "Unknown error";
+		std::string description = "No description";
+
+		// Decode the error code
+		switch (errorCode)
+		{
+			case GL_INVALID_ENUM:
+			{
+				error = "GL_INVALID_ENUM";
+				description = "An unacceptable value has been specified for an enumerated argument.";
+				break;
+			}
+
+			case GL_INVALID_VALUE:
+			{
+				error = "GL_INVALID_VALUE";
+				description = "A numeric argument is out of range.";
+				break;
+			}
+
+			case GL_INVALID_OPERATION:
+			{
+				error = "GL_INVALID_OPERATION";
+				description = "The specified operation is not allowed in the current state.";
+				break;
+			}
+
+			case GL_STACK_OVERFLOW:
+			{
+				error = "GL_STACK_OVERFLOW";
+				description = "This command would cause a stack overflow.";
+				break;
+			}
+
+			case GL_STACK_UNDERFLOW:
+			{
+				error = "GL_STACK_UNDERFLOW";
+				description = "This command would cause a stack underflow.";
+				break;
+			}
+
+			case GL_OUT_OF_MEMORY:
+			{
+				error = "GL_OUT_OF_MEMORY";
+				description = "There is not enough memory left to execute the command.";
+				break;
+			}
+
+			case GL_INVALID_FRAMEBUFFER_OPERATION_EXT:
+			{
+				error = "GL_INVALID_FRAMEBUFFER_OPERATION";
+				description = "The object bound to FRAMEBUFFER_BINDING is not \"framebuffer complete\".";
+				break;
+			}
+		}
+
+		// Log the error
+		std::cout << "An internal OpenGL call failed in " << file << "(" << line << ")."
+			<< "\nExpression:\n   " << function << "\nError description:\n   " << error << "\n   " << description << '\n'
+			<< std::endl;
 		return false;
 	}
 	return true;
--- LearnOpenGL/src/Utils.h
+++ LearnOpenGL/src/Utils.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include <GL/glew.h>
@@ -12,7 +13,7 @@
 #define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
 void GLClearError();
-bool GLLogCall(const char* function, const char* file, int line);
+bool GLLogCall(std::string_view function, const char* file, int line);
 unsigned int LoadTexture(const std::string& path);
 unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma = false);
 unsigned int LoadCubeMap(std::vector<std::string> faces, std::string relPath);
